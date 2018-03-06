d6dadd1baa2733df440f183d8defe4ad23f607e6
Jessica Ahl <jessahl@gmail.com>
2018-03-06 14:31:09 -0800

    user landing page

--- public/src/app/pets/pet-list/pet-list.component.ts
+++ public/src/app/pets/pet-list/pet-list.component.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Pet } from '../../pets';
 import { User } from '../../users';
 import { DataService } from '../../data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pet-list',
   templateUrl: './pet-list.component.html',
   styleUrls: ['./pet-list.component.css']
 })
+
 export class PetListComponent implements OnInit {
+  newPet: Pet = new Pet();
   allPets: Array<Pet>;
-  currentUser:any = {firstName:'req.params.firstName', lastName:'req.params.lastName',}
+  currentUser:any = {firstName:'req.params.firstName', lastName:'req.params.lastName', id:'req.params._id'}
+  hasImage: boolean = false;
+  myPet: Array<Pet>;  
   searchStr: string = '';
 
-  constructor(private _dataService: DataService) { 
-  }
+  constructor(private _dataService: DataService, private _router: Router) { }
 
   ngOnInit() {
     this.getAllPets();
@@ -26,6 +30,29 @@ export class PetListComponent implements OnInit {
       this.allPets= dataFromService;
       console.log(this.allPets);
 		})
+  }
+  
+  getPets() {
+  this._dataService.getAllUsersPets()
+  }
+  
+  linkPhoto() {
+    this.newPet.image = prompt("Please enter the url for your photo:");
+    if (this.newPet.image != "") {
+      this.hasImage = true;
+    }
+  }
+  addPet() {
+    this._dataService.postPet(this.currentUser._id, this.newPet, ()=>{
+      console.log(this);
+      this._router.navigate(['/']);
+    })
+    this.newPet = new Pet();
+    console.log('pet added')
+  }
+
+  updatePet(pet) {
+    this._dataService.update(pet)
   }
 
   deletePet(pet){
